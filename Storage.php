b3fb1a6ccdb739cff56703c11346f730d8286e2f
Nihal Chirayath <chirayathnihal@gmail.com>
2017-03-14 20:31:27 +0530

    Made images offline , added storage.php , Small ui changes

---
+++ Storage.php
@@ -0,0 +1,133 @@
+<?php
+include 'connect.php' ;
+?>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="icon.png" />
+  <title>Jai Kisan - Home</title>
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+  <div class="site-outer">
+	<nav class="navbar navbar-fixed-top">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="container-menu">
+					<nav class="menu" id="hb">
+						<div></div>
+						<div></div>
+						<div></div>
+					</nav>
+					<div class="logotype">
+						<div class="big hero">Jai Kisan</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</nav>
+  <div class="row mg">
+    <!-- FIRST ROW -->
+    <div class="container">
+      <h1 class="text-center head red">Useful Schemes For Farmers!</h1>
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="product-box">
+            <h2 class="text-center"><i class="fa fa-flask"></i></h2>
+            <h3 class="text-center">Archetype Lab</h3>
+            <p>Competition breeds innovation, fact. So while the snowboard industry might be having a bit of a hard time off the back of a terrible winter season for snow in the States and a global recession that the world economy just can’t shake, increased
+              competition for board sales is driving innovation in Snowboard design and construction. It’s a pretty good time to be a consumer right now, with brands taking the brunt of materials, logistic and labour cost inflation but having to pump more
+              and more revenue into RnD to keep their products desirable.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="product-box">
+            <h2 class="text-center"><i class="fa fa-balance-scale"></i></h2>
+            <h3 class="text-center">World’s Lightest Split</h3>
+            <p>Last February at ISPO Jeremy Jones announced the Ultracraft, a splitboard so light that it skimmed at least half a kilogram off its closest rival. Saving weight is even more important to splitboarders than it is to standard snowboarders because
+              as splitboarding folk law goes, one gram on your feet is the equivalent of three on your back. So mr Jones must be feeling a little miffed that Amplid has released a splitboard a whole 200 grams lighter before the Ultracraft even hit the shop
+              racks. The Amplid LAB Carbon split weighs in at an incredible 2.3kg.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="footer">
+  <footer class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h3 class="h3">About us</h3>
+          <ul class="footer-list">
+            <li><a href="#">Our story</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Partners</h3>
+          <ul class="footer-list">
+            <li><a href="https://github.com/GK4444">Gaurav Kavhar</a></li>
+            <li><a href="https://github.com/NiC24">Nihal Chirayath</a></li>
+            <li><a href="https://github.com/vishalzyy">Vishal Prajapat</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Customer support</h3>
+          <ul class="footer-list">
+            <li>Chat with us</li>
+            <li>+91 2453 352</li>
+            <li>E-mail us</li>
+            <li>subtestadd@gmail.com</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Socials</h3>
+          <ul class="footer-list footer-list-inline">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+<!-- NAVIGATION -->
+<div class="navigation">
+  <div class="navigation-content">
+    <h3>Menu</h3>
+    <ul class="navigation-menu">
+      <li>Your Here : <a href="#" class="active">Storage Spaces</a></li>
+      <li><a href="index.php"><i class="orange fa fa-circle"></i>Home</a></li>
+      <li><a href="#"><i class="red fa fa-circle"></i>Farmer's Rights</a></li>
+      <li><a href="#"><i class="beige fa fa-circle"></i>Major Crops & Markets</a></li>
+      <li><a href="#"><i class="blue fa fa-circle"></i>New Technology</a></li>
+    </ul>
+    <h3>More</h3>
+    <ul class="navigation-menu">
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Info</a></li>
+      <li><a href="#">Customer Service</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.0/handlebars.min.js'></script>
+<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+
+  <script src="js/index.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -38,7 +38,7 @@ include 'connect.php' ;
 			<!-- ACCORDION ROW -->
 			<div class="row">
 					<ul class="accordion-group" id="accordion">
-						<li style="background-image: url('http://www.thedialogue.co/wp-content/uploads/2016/10/farmers-1.jpg');">
+						<li style="background-image: url('farmer1.jpg');">
 							<div class="accordion-overlay"></div>
 							<h3>Farmer's Rights</h3>
 							<section class="hidden-xs">
@@ -47,7 +47,7 @@ include 'connect.php' ;
 								</article>
 							</section>
 						</li>
-						<li class="out" style="background-image: url('https://t1.uc.ltmcdn.com/en/images/0/7/3/img_crops_grown_in_rabi_season_3370_orig.jpg');">
+						<li class="out" style="background-image: url('orig.jpg');">
 							<div class="accordion-overlay"></div>
 							<h3>Major Crops & Markets</h3>
 							<section class="hidden-xs">
@@ -57,16 +57,16 @@ include 'connect.php' ;
 								</article>
 							</section>
 						</li>
-						<li style="background-image: url('http://www.knowledgebank.irri.org/images/stories/postharvest-storage.jpg');">
+						<li style="background-image: url('storage.jpg');">
 							<div class="accordion-overlay"></div>
-							<h3>Storage Spaces</h3>
+							<h3><a href="Storage.php">Storage Spaces</a></h3>
 							<section class="hidden-xs">
 								<article>
-									<p>Have large stock of crops ?  dont have enough space to store ?.... no need to worry just click here and see the storage spaces available in your district</p>
+									<p><a href="Storage.php">Have large stock of crops ?  dont have enough space to store ?.... no need to worry just click here and see the storage spaces available in your district</a></p>
 								</article>
 							</section>
 						</li>
-						<li style="background-image: url('http://smartfarming.co.za/wp-content/uploads/2013/10/dfsgs.jpg');">
+						<li style="background-image: url('dfsgs.jpg');">
 							<div class="accordion-overlay"></div>
 							<h3>New Technolgy</h3>
 							<section class="hidden-xs">
@@ -160,10 +160,10 @@ include 'connect.php' ;
 		<div class="navigation-content">
 			<h3>Menu</h3>
       <ul class="navigation-menu">
-				<li><a href="#" class="active">Home</a></li>
-				<li><a href="#"><i class="red fa fa-circle"></i>Fertilizers & Manures</a></li>
+				<li>Your Here : <a href="#" class="active">Home</a></li>
+				<li><a href="#"><i class="red fa fa-circle"></i>Farmer's Rights</a></li>
 				<li><a href="#"><i class="beige fa fa-circle"></i>Major Crops & Markets</a></li>
-				<li><a href="#"><i class="orange fa fa-circle"></i>Farmer's Rights</a></li>
+				<li><a href="Storage.php"><i class="orange fa fa-circle"></i>Storage Spaces</a></li>
 				<li><a href="#"><i class="blue fa fa-circle"></i>New Technology</a></li>
 			</ul>
 			<h3>More</h3>
