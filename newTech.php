2f102e3a0c47ca4ac0184e2728f910dc43891acd
GK <GK>
2017-03-15 23:48:47 +0530

    New Technology page created

---
+++ newTech.php
@@ -0,0 +1,210 @@
+<?php
+include 'connect.php' ;
+?>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="icon.png" />
+  <title>Jai Kisan - Home</title>
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+  <div class="site-outer">
+	<nav class="navbar navbar-fixed-top">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="container-menu">
+					<nav class="menu" id="hb">
+						<div></div>
+						<div></div>
+						<div></div>
+					</nav>
+					<div class="logotype">
+						<div class="big hero"><a href="index.php">Jai Kisan</a></div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</nav>
+  <div class="site-inner">
+    <section class="container-fluid">
+  <div class="row mg">
+    <!-- FIRST ROW -->
+    <div class="container">
+      <h1 class="text-center head red">New Technologies in Farming</h1><br>
+          <div>
+            <h3 class="text"><a href="http://www.barrix.in/">1. Barrix Ago Sciences</h3><br>
+			<img src="1.jpg" alt="Barrix Ago Sciences" style="vertical-align:middle" >
+            <br><p>The Bangalore-based startup offers eco-friendly crop protection methods after much research on products that support organic farming to increase crop produce and quality with minimal expenditure.
+<br><br>
+Products:<br>Barrix Catch Fruit and Fly Lure + trap: Toxic pesticides contaminate water, soil and leave behind harmful residue, besides being expensive. Barrix’s pheromone-based pest control traps have artificially synthesised smelling agents that attracts and traps pests. Instead of eating the crops, the pests are attracted to the pheromones in the traps.
+Fly pest sticky sheet: Barrix uses bright yellow and blue coloured recyclable sheets of wavelengths between 500 nm to 600 nm, proven to effectively attract and trap at least 19 high-risk pests from a long distance.
+</p>
+          </div>
+		   <div>
+            <h3 class="text"><a href="http://www.anulekh.com/">2. Anulek Agrotech</h3><br>
+			<img src="2.jpg" alt="Anulek Agrotech" style="vertical-align:middle" >
+            <br><p>Set up by Mumbai-based entrepreneurs, Anulekh focuses on increasing soil fertility to achieve higher agricultural productivity and crop yield with lower resource use.
+<br><br>
+Products:<br>BIOSAT: BIOSAT (Biochar based organic Soil Amendment Technology), a soil additive, is made of biochar mixed with different organic nutrients. The product preserves soil fertility, traps carbon emissions, maintains the topsoil strength and increases crop production, thus reducing dependency on chemical fertilizers.
+</p>
+          </div>
+		  <div>
+            <h3 class="text"><a href="http://mitraweb.in/">3. Mitra</h3><br>
+			<img src="3.jpg" alt="Mitra" style="vertical-align:middle" >
+            <br><p>A Nashik-based startup, MITRA (Machines, Information, Technology, Resources for Agriculture) aims to improve mechanization at horticulture farms with the use of R&D and high quality farm equipment.
+<br><br>
+Products:<br>Air blast sprayers: Developed for fruits and vegetables in general, and grapes and pomegranates in particular, the sprayers, used to add hormones that help the growth of crops, reduce the expenditure on manual labour and are less time-consuming.
+</p>
+          </div>
+		    <div>
+            <h3 class="text"><a href="http://cropin.co.in/">4. CropIn Technology Solutions</h3><br>
+			<img src="4.png" alt="CropIn Technology Solutions" style="vertical-align:middle" >
+            <br><p>A farming technology solutions startup founded by a Bangalore software engineer, it provides agri businesses the technology and expertise to create a smarter and safer food supply for consumers around the world.
+<br><br>
+			Products:<br>CropIn offers information on a cloud-based platform, integrated with a mobile app for Android. Called Smart Farms, it allows large food companies to track the growth of crops on farms around the country with details about what the crop is and the conditions it is grown in to help companies remotely monitor farms, interact with farmers and make every crop transparents and traceable. It also aids farmers in adopting global agricultural practices and improves productivity by offering productivity insights and harvest forecasts.
+</p>
+          </div>
+		     	    <div>
+            <h3 class="text"><a href="http://www.eruvaka.com/">5. Eruvaka Technologies</h3><br>
+			<img src="5.jpg" alt="5. Eruvaka Technologies" style="vertical-align:middle" >
+            <br><p>An organisation based in Vijayawada, Andhra Pradesh, its mission is to accelerate the use of technology in aquaculture, an area where farmers face problems due to unavailability of adequate technology to measure and control water health.
+			<br><br>
+Products:<br>Eruvaka Technologies, to help farmers monitor aquaculture ponds, develops solar-powered flouting buoys that measure different water parameters, such as oxygen levels, temperature and pH range, crucial for the growth and survival of fish and shrimp. The collected information is uploaded on the cloud and transmitted to individual customers through an Android app, SMS, voice call or the internet. Farmers can also remotely control automated equipment such as aerators and feeders.
+</p>
+          </div>
+		    <div>
+            <h3 class="text"><a href="http://www.skymetweather.com/corporate/">6. Skymet</h3><br>
+			<img src="6.png" alt="6. Skymet" style="vertical-align:middle" >
+            <br><p>Skymet is India’s largest weather monitoring and agri-risk solutions company. According to their website, they are the experts in measuring, predicting, and limiting climate risk to agriculture, thus reducing losses incurred due to bad weather conditions.
+			<br><br>
+Products:<br>Launched to aid farmers, Skymet’s weather website offers services such as weather forecast, crop insurance and agri-risk management. Prediction of weather conditions can help prepare a farmers for a drought or heavy unseasonal rainfall and help them take appropriate preventive measures, they say and claim to accurately measure and predict yield at the village level for any crop.
+</p>
+          </div>
+		         
+		    <div>
+            <h3 class="text"><a href="https://www.ekgaon.com/index.php">7. Ekgaon </h3><br>
+			<img src="7.jpg" alt="7. Ekgaon" style="vertical-align:middle" >
+            <br><p>A Gujarat-based venture started in 2001, Ekgaon Technologies is an IT based network integrator that provides a technology platform and offers a range of services to farmers in rural areas including financial, agricultural inputs and government assistance.
+			<br><br>
+Products:<br>Financial: A mobile phone enabled financial services delivery platform, it provides information on microfinance institutions and banks for delivery of door-step services such as credit, savings, remittance, insurance, investment and mortgage.
+<br>Agricultural: Offered in Hindi, Gujarati and Tamil languages, the system uses mobile, voice recognition, interactive voice response system (IVRS) and web technologies to provide information on weather, commodity market prices, soil nutrient management and crop management.
+<br>Citizen: The web and mobile applications help citizens monitor the delivery of government programmes and services entitled to them.
+</p>
+          </div>
+		       
+		    <div>
+            <h3 class="text"><a href="http://www.digitalgreen.org/">8. Digital Green</h3><br>
+			<img src="8.jpg" alt="8. Digital Green" style="vertical-align:middle" >
+            <br><p>development organisation that focuses on training farmers to make and show short videos where they record their problems, share solutions and highlight success stories as community engagement to improve lives of rural communities across South Asia and Sub-Saharan Africa.
+			<br><br>
+Products:<br>It uses technology-enabled behaviour change communication that is cost-effective, scalable and brings together researchers, development practitioners, and rural communities to produce and share locally relevant information through videos.
+<br>Two social online games Wonder Village and Farmer Book: In the games, players simulate a village economy and relate with actual farmers that Digital Green works with, on the field. The players are placed in a resource-constrained setting in which they have to complete quests such as set up paddy and maize farms and supply raw materials to the farmers’ markets.
+</p>
+          </div>
+		        
+		    <div>
+            <h3 class="text"><a href="http://www.frontalrain.com/">9. FrontalRain Technologies</h3><br>
+			<img src="9.png" alt="9. FrontalRain Technologies" style="vertical-align:middle" >
+            <br><p>The Bangalore-based agri-tech startup seeks to deliver affordable advanced technology solutions for emerging companies and take technology to remote corners of the country.
+			<br><br>
+Products:<br>The company’s offering Rain+, according to their website, is a comprehensive suite of products on the cloud for food and agribusinesses. Rain+ can help companies at every stage of the value chain starting from growing, processing, logistics, wholesale trade, retail trade and exports. This technology, accessible through desktop, tablet and mobile devices, is used by companies dealing with commodities like spices, herbs, basmati rice, seeds, animal feed, sea food, dairy and edible oil.
+</p>
+          </div>
+		        
+		    <div>
+            <h3 class="text"><a href="http://agrostar.in/">10. Agrostar </h3><br>
+			<img src="10.jpg" alt="10. Agrostar " style="vertical-align:middle" >
+            <br><p>A Pune-based ‘direct to farmer’ m-commerce platform, Agrostar strives to provide quality agro inputs at the farmers’ doorstep.
+			<br><br>
+Products:<br>AgroStar enables farmers to procure a range of agricultural goods such as seeds, crop nutrition, crop protection and agri-hardware products by simply giving a missed call on the company’s 1800 number or through their mobile app to eliminate unavailability of products, substandard products, duplication and adulteration.
+</p>
+          </div>
+        
+		</div>
+      </div>
+    </div>
+  </div>
+
+<div class="footer">
+  <footer class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h3 class="h3">About us</h3>
+          <ul class="footer-list">
+            <li><a href="#">Our story</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Partners</h3>
+          <ul class="footer-list">
+            <li><a href="https://github.com/GK4444">Gaurav Kavhar</a></li>
+            <li><a href="https://github.com/NiC24">Nihal Chirayath</a></li>
+            <li><a href="https://github.com/vishalzyy">Vishal Prajapat</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Customer support</h3>
+          <ul class="footer-list">
+            <li>Chat with us</li>
+            <li>+91 2453 352</li>
+            <li>E-mail us</li>
+            <li>subtestadd@gmail.com</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3 class="h3">Socials</h3>
+          <ul class="footer-list footer-list-inline">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+</section>
+</div>
+<!-- NAVIGATION -->
+<div class="navigation">
+  <div class="navigation-content">
+    <h3>Menu</h3>
+    <ul class="navigation-menu">
+      <!-- //Topic title here -->
+      <li>Your are at <a href="#" class="active">New Technology</a></li>
+      <!-- // other Pages below -->
+      <li><a href="index.php"><i class="orange fa fa-circle"></i>Home</a></li>
+      <li><a href = "farmerrights.php"><i class="red fa fa-circle"></i>Farmer's Rights</a></li>
+      <li><a href="crops.php"><i class="beige fa fa-circle"></i>Major Crops & Markets</a></li>
+      <li><a href="newTech.php"><i class="blue fa fa-circle"></i>New Technology</a></li>
+    </ul>
+    <h3>More</h3>
+    <ul class="navigation-menu">
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Info</a></li>
+      <li><a href="#">Customer Service</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.0/handlebars.min.js'></script>
+<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+
+  <script src="js/index.js"></script>
+
+</body>
+</html>
