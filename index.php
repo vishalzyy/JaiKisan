517681a3375c4e02c35a2a4b2d4cec6f058f5e85
Gaurav Kavhar <gauravkavhar304@gmail.com>
2017-03-16 23:11:20 +0530

    Schemes added

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ include 'connect.php' ;
 							<section class="hidden-xs">
 								<article>
 									<p><a href="newTech.php">Find technology which help you increase productivity and minimize your effort.</a></p>
-		
+
 								</article>
 							</section>
 						</li>
@@ -83,33 +83,35 @@ include 'connect.php' ;
 			<div class="row mg">
 				<!-- FIRST ROW -->
 				<div class="container">
-					<h1 class="text-center head red">Useful Schemes For Farmers!</h1>
+					<h1 class="text-center head red">Government Schemes For Farmers!</h1>
 					<div class="row">
 						<div class="col-md-4 col-sm-6 col-xs-12">
 							<div class="product-box">
 								<h2 class="text-center"><i class="fa fa-gears"></i></h2>
-								<h3 class="text-center">High quality products</h3>
-								<p>Nose shaping is the new reverse camber. Blunted, pointy, asymmetric… whatever, as long as it’s not round its in-Vogue for next winter. It’s not all about aesthetics, a number of brands are increasing nose length and kick radius to increase surface
-									area and improve floatation and following the lead of last year’s K2 Ultradream, which undoubtedly borrowed the concept from 5D ski shaping. The Nitro Uberspoon and Lib Tech Speedodeeps Travis Rice are good examples of snowboards where effective
-									edge has been sacrificed for surface area.</p>
+								<h3 class="text-center"><a href="http://dahd.nic.in">Rashtriya Gokul Mission</a></h3>
+								<p><br>Launched: ‎16 December 2014<br>
+Main Objective: To conserve and develop indigenous bovine breeds.<br>
+Rashtriya Gokul Mission aims to conserve and develop indigenous breeds in a focused and scientific manner. It is a focussed project under National Programme for Bovine Breeding and Dairy Development, with an outlay of Rs 500 crore during the 12th Five Year Plan.</p>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 col-xs-12">
 							<div class="product-box">
 								<h2 class="text-center"><i class="fa fa-flask"></i></h2>
-								<h3 class="text-center">Archetype Lab</h3>
-								<p>Competition breeds innovation, fact. So while the snowboard industry might be having a bit of a hard time off the back of a terrible winter season for snow in the States and a global recession that the world economy just can’t shake, increased
-									competition for board sales is driving innovation in Snowboard design and construction. It’s a pretty good time to be a consumer right now, with brands taking the brunt of materials, logistic and labour cost inflation but having to pump more
-									and more revenue into RnD to keep their products desirable.</p>
+								<h3 class="text-center"><a href="http://www.soilhealth.dac.gov.in">Soil Health Card Scheme</a></h3>
+								<p><br>Launched: 17 February 2015<br>
+Main Objective: To help farmers to improve productivity from their farms by letting them know about nutrient/fertilizer requirements for their farms.<br>
+The soil health card studies and reviews the health of soil or rather we can say a complete evaluation of the quality of soil right from its functional characteristics, to water and nutrients content and other biological properties. It will also contain corrective measures that a farmer should adopt to obtain a better yield.
+</p>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 col-xs-12">
 							<div class="product-box">
 								<h2 class="text-center"><i class="fa fa-balance-scale"></i></h2>
-								<h3 class="text-center">World’s Lightest Split</h3>
-								<p>Last February at ISPO Jeremy Jones announced the Ultracraft, a splitboard so light that it skimmed at least half a kilogram off its closest rival. Saving weight is even more important to splitboarders than it is to standard snowboarders because
-									as splitboarding folk law goes, one gram on your feet is the equivalent of three on your back. So mr Jones must be feeling a little miffed that Amplid has released a splitboard a whole 200 grams lighter before the Ultracraft even hit the shop
-									racks. The Amplid LAB Carbon split weighs in at an incredible 2.3kg.</p>
+								<h3 class="text-center"><a href="http://www.nsiindia.gov.in">Kisan Vikas Patra</a></h3>
+								<p><br>Launched: 3 March 2015 (Re-Launched)<br>
+Main Objective: To provide safe and secure investment avenues to the small investors.<br>
+Kisan Vikas Patra is an investment scheme wherein the invested money will get doubled in 8 years and 4 months. However, investors would not get any tax benefit for their investment in Kisan Vikas Patra unlike in PPF. The Kisan Vikas Patra certificates would be available in the denominations of Rs 1,000, 5,000, 10,000 and 50,000 and there is no upper limit on investment in KVPs.
+</p>
 							</div>
 						</div>
 					</div>
