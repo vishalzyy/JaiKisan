18f5abaf3593085115faf7894f4f386cd369d144
Nihal Chirayath <chirayathnihal@gmail.com>
2017-03-16 23:56:24 +0530

    deleted the dynamic list

--- dynamic list/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname  = 'localhost';
-$username = 'root';
-$password='';
-$dbname = 'jaikisan';
-$connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
-?>
